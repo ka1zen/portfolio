44b821802f761de73191bea84f95007d3a4b82d4
Lyes <lyes.sadmi@tictactrip.eu>
2019-04-28 14:20:13 +0200

    portfolio functionnal

---
+++ jobs/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+
+
+# Create your views here.
+def homepage(request):
+    jobs = Job.objects.all()
+    context = {
+        'jobs': jobs
+    }
+    return render(request, 'jobs/home.html', context)
+
+
+def detail(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/detail.html', {'job': job})
